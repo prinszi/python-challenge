56a88e480bcfd3469bec977af6f0203cacfa2c9b
Prinszi <prins.schultz@gmail.com>
2019-06-05 12:41:46 -0500

    Updated commit

---
+++ PyBank/main.py
@@ -0,0 +1,51 @@
+import os
+import csv
+
+date = []
+money = []
+total_months = 0
+total_money = 0
+grt_inc = 0
+grt_dec = 0
+
+csvpath = os.path.join('budget_data.csv')
+
+with open(csvpath, newline='') as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    for row in csvreader:
+        date.append(row[0])
+        money.append(row[1])
+        total_months = total_months + 1
+        total_money = total_money + int(row[1])
+        if int(row[1]) > int(grt_inc):
+            grt_inc = row[1]
+            inc_month = str(row[0])
+        if int(row[1]) < int(grt_dec):
+            grt_dec = row[1]
+            dec_month = str(row[0])
+        
+avg_change = total_money/total_months
+
+output_file = "test.txt"
+
+output = (
+    f"\nFinancial Analysis\n"
+    f"----------------------------\n"
+    f"Total Months: {total_months}\n"
+    f"Total Revenue: ${total_money}\n"
+    f"Average Revenue Change: ${avg_change}\n"
+    f"Greatest Increase in Revenue: {inc_month} (${grt_inc})\n"
+    f"Greatest Decrease in Revenue: {dec_month} (${grt_dec})\n")
+
+with open(output_file, "w") as txt_file:
+    txt_file.write(output)
+
+print(output)
+
